b01d714eb0c4c4786c966f212b90fe888c19141d
mulondu <mulondumuema@gmail.com>
2021-08-16 06:20:30 +0300

    updated

---
+++ go_crash_course/string-interpolation/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// var reader *bufio.Reader
+type User struct {
+	UserName string
+	Age int
+	FavouriteNumber float64
+}
+
+
+func main(){
+	// reader := bufio.NewReader(os.Stdin)
+
+	var user User
+	user.UserName = readString("What is your name?")
+	user.Age = readInt("How old are you?")
+	user.FavouriteNumber = readFloat("What is your favorite number?")
+
+
+	fmt.Printf("Your name is %s. You are %d years old. And your favorite number is %.2f.\n", 
+		user.UserName,
+		user.Age,
+		user.FavouriteNumber)
+
+
+	
+}
+
+func prompt() {
+	fmt.Print("->")
+}
+
+
+func readString(s string) string {
+	for {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println(s)
+	prompt()
+
+	userInput, _ := reader.ReadString('\n')
+	userInput = strings.Replace(userInput, "\r\n", "", -1)
+	userInput = strings.Replace(userInput, "\n", "", -1)
+
+	if userInput == "" {
+		fmt.Println("Please enter a value")
+	} else {
+		return userInput
+	} 
+	
+	}
+}
+
+func readInt(s string) int{
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println(s)
+		prompt()
+
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		num, err := strconv.Atoi(userInput)
+		if err != nil {
+			fmt.Println("Please enter a whole number")
+		} else{
+			return num
+		}
+	}
+
+}
+
+func readFloat(s string) float64{
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println(s)
+		prompt()
+
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		num, err := strconv.ParseFloat(userInput,64)
+		if err != nil {
+			fmt.Println("Please enter a number")
+		} else{
+			return num
+		}
+	}
+
+}
